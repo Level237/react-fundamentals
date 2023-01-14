3a5000610ab0e851a66144fcb286c42e58a97857
level237 <Bramslevel129@gmail.com>
2023-01-14 14:35:29 +0100

    using fragment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-function App(props){
+import Navbar from "./Navbar";
+import React from "react";
+function App(){
     return (
+        <React.Fragment>
+            <Navbar title="Using Fragment" />
+        
         <div className="bg-dark">
             Hello World!
         </div>
+        </React.Fragment>
     )
 }
 
