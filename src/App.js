ec2b2f67c39d2dd3e63b025175557c1f62d8da91
level237 <Bramslevel129@gmail.com>
2023-01-16 08:31:22 +0100

    Getting Started with state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import List from "./List";
+import Counter from "./Counter";
 function App(){
 
     const guitars=['Strat','Les Paul','Explorer'];
@@ -7,13 +8,15 @@ function App(){
 
     return (
         <>
-            <Navbar title="Css and Separating JS and JSX" />
+            <Navbar title="Getting Started with state" />
         
         <div className="container">
-            <List
+
+            <Counter />
+            {/* <List
             title="Guitars"
             background="danger"
-            />
+            /> */}
         </div>
         </>
     )
