3697baa48dcf01b128ef0ba6872f0d09f4cab391
level237 <Bramslevel129@gmail.com>
2023-01-16 12:18:15 +0100

    Component as Class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import List from "./List";
-import Counter from "./Counter";
+import Counter from "./CounterClass";
 function App(){
 
     const guitars=['Strat','Les Paul','Explorer'];
