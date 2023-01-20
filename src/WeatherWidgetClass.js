8011582334dd55453b41e34f7b570209fa44fe4b
level237 <Bramslevel129@gmail.com>
2023-01-20 13:52:21 +0100

    Handling side effects in class Components

--- src/WeatherWidgetClass.js
+++ src/WeatherWidgetClass.js
@@ -1,18 +1,34 @@
 import { Component } from "react";
-
 class WeatherWidget extends Component {
 
     constructor(props){
 
         super(props);
+
+        this.state={
+            data:null
+        };
+    }
+
+    componentDidMount(){
+        if(this.props.location){
+            console.log('request made in componentDidMount');
+        }
+        
+    }
+
+    componentDidUpdate(prevProps,prevState,snapshot){
+        if(prevProps.location ===! this.props.location){
+            console.log("request made in componentDidUpdate");
+        }
     }
     
     render(){
+        console.log("render occured");
         return (
-            <div>
-                
-            </div>
+            <div></div>
         )
     }
 }
+
 export default WeatherWidget;
